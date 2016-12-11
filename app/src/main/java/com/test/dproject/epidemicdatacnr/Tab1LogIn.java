084fbf769e8ec61d6251d7fa28ea5dee930885b7
SuKSW <subhashinieksw.14@cse.mrt.ac.lk>
2016-12-11 11:49:12 +0530

    With intent to launch patients registration activity

--- app/src/main/java/com/test/dproject/epidemicdatacnr/Tab1LogIn.java
+++ app/src/main/java/com/test/dproject/epidemicdatacnr/Tab1LogIn.java
@@ -4,11 +4,15 @@ package com.test.dproject.epidemicdatacnr;
  * Created by Subhashinie on 12/3/2016.
  */
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 
 public class Tab1LogIn extends Fragment{
 
@@ -16,6 +20,18 @@ public class Tab1LogIn extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab1_log_in, container, false);
+        final Button pRegButton = (Button) rootView.findViewById(R.id.pRegButton);
+
+        pRegButton.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent pReg = new Intent(getActivity(), patientRegistration.class);
+                        getActivity().startActivity(pReg);
+                    }
+                }
+        );
         return rootView;
     }
+
+
 }
---
+++ app/src/main/java/com/test/dproject/epidemicdatacnr/patientRegistration.java
@@ -0,0 +1,13 @@
+package com.test.dproject.epidemicdatacnr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class patientRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_registration);
+    }
+}
