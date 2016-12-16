7bcbbea0ab1b4bb064fd45d66a980e7f2fc6cff3
Pubudu Cooray <pubuduc.14@cse.mrt.ac.lk>
2016-12-17 01:29:04 +0530

    adding interfaces

--- app/src/main/java/com/test/dproject/epidemicdatacnr/Tab1LogIn.java
+++ app/src/main/java/com/test/dproject/epidemicdatacnr/Tab1LogIn.java
@@ -5,13 +5,13 @@ package com.test.dproject.epidemicdatacnr;
  */
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class Tab1LogIn extends Fragment{
@@ -19,10 +19,12 @@ public class Tab1LogIn extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.tab1_log_in, container, false);
+        final View rootView = inflater.inflate(R.layout.tab1_log_in, container, false);
 
         final Button loginButton = (Button) rootView.findViewById(R.id.loginButton);
         final Button pRegButton = (Button) rootView.findViewById(R.id.pRegButton);
+        final Button dRegButton = (Button) rootView.findViewById(R.id.dRegButton);
+        final Button mRegButton = (Button) rootView.findViewById(R.id.mRegButton);
 
 
         loginButton.setOnClickListener(
@@ -39,9 +41,28 @@ public class Tab1LogIn extends Fragment{
                     public void onClick(View v) {
                         Intent pReg = new Intent(getActivity(), patientRegistration.class);
                         getActivity().startActivity(pReg);
+                        Toast.makeText(rootView.getContext(),"Register as a patient",Toast.LENGTH_SHORT).show();
                     }
                 }
         );
+
+        dRegButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dReg = new Intent(getActivity(),DoctorRegistration.class);
+                getActivity().startActivity(dReg);
+                Toast.makeText(rootView.getContext(), "Register as a doctor", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mRegButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mReg = new Intent(getActivity(),MedicalOfficerRegistration.class);
+                getActivity().startActivity(mReg);
+                Toast.makeText(rootView.getContext(),"Register as a medical officer",Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return rootView;
     }
 
